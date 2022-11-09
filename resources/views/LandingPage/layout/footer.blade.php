752cd42681a28583f547c974493afe3944372bf8
Raheel Anwaar <raheelanwaar0316@gmail.com>
2022-11-09 23:08:38 +0500

    layout + landing Page is shown through Controller

---
+++ resources/views/LandingPage/layout/footer.blade.php
@@ -0,0 +1,16 @@
+
+    <script src="{{ asset('assets/js/jquery-3.5.0.min.js') }}"></script>
+    <script src="{{ asset('assets/js/styleswitcher.js') }}"></script>
+    <script src="{{ asset('assets/js/preloader.min.js') }}"></script>
+    <script src="{{ asset('assets/js/fm.revealator.jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/js/masonry.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/js/classie.js') }}"></script>
+    <script src="{{ asset('assets/js/cbpGridGallery.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery.hoverdir.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,13 @@
 <?php
 
+use App\Http\Controllers\LandingPage\LandingPageController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Landing Page Routes
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/',[LandingPageController::class,'landing'])->name('LandingPage');
+
+
 
 require __DIR__.'/auth.php';
